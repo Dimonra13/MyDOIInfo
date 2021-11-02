0dba542789fefe43bbb1b5a1303ad70a3235bcec
dimonra13 <diegomontoto4@hotmail.com>
2021-11-02 15:41:26 +0100

    Project created

---
+++ src/main/java/tfg/urjc/mydoiinfo/MyDoiInfoApplication.java
@@ -0,0 +1,13 @@
+package tfg.urjc.mydoiinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDoiInfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyDoiInfoApplication.class, args);
+    }
+
+}
